b052a445bc7ef1d2402142e75736e5e2a51ed472
Thet_Naing <144353254+thet-naing-lin@users.noreply.github.com>
2024-08-23 20:38:48 +0630

    React Day 10 Homework - fetch data and show it in table

--- 10.api/main.js
+++ 10.api/main.js
@@ -1,6 +1,47 @@
 import "./style.css";
+// import "./node_modules/flowbite/dist/flowbite"
 
 const fetchBtn = document.querySelector("#fetchBtn");
+const app = document.querySelector("#app");
+const taskTable = document.querySelector("#taskTable");
+const tableBody = document.querySelector("#tableBody");
+const taskRowTemplate = document.querySelector("#taskRowTemplate");
+
+const createRow = (tasks) => {
+  tasks.forEach(({title, description, status}) => {
+      const taskRow = taskRowTemplate.content.cloneNode(true);
+
+      const taskTitle = taskRow.querySelector("#task-title");
+      const taskDescription = taskRow.querySelector("#task-description");
+      const taskStatus = taskRow.querySelector("#task-status");
+
+      taskTitle.innerText = title;
+      taskDescription.innerText = description;
+      taskStatus.innerText = status;
+
+      tableBody.append(taskRow);
+  })
+}
+
+// const showTasks = (tasks) => {
+//   tasks.forEach((task) => {
+//     // console.log(task);
+//     const body = document.createElement("tbody");
+//     body.innerHTML = `<tr
+//               class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
+//             >
+//               <th
+//                 scope="row"
+//                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+//               >
+//                ${task.title}
+//               </th>
+//               <td class="px-6 py-4">${task.description}</td>
+//               <td class="px-6 py-4">${task.status}</td>
+//             </tr>`;
+//     taskTable.append(body);
+//   });
+// };
 
 const handleFetchBtn = () => {
   console.log("click");
@@ -25,9 +66,10 @@ const handleFetchBtn = () => {
   fetch("http://localhost:5100/tasks")
     .then((res) => res.json())
     .then((data) => {
-      data.forEach((data) =>
-        console.log(data.title, " | ", data.description, " | ", data.status)
-      );
+      // data.forEach((data) =>
+      //   console.log(data.title, " | ",x data.description, " | ", data.status)
+      // );
+      createRow(data);
     });
 
   // fetch("http://localhost:5100/tasks/3")
